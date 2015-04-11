c0629ec1e52da7da37dc43eca319ef802a70a244
Daniela Gattoni <danigattoni@gmail.com>
2015-04-11 15:08:58 -0300

    created mapviewer controller and view

---
+++ app/controllers/mapviewer_controller.rb
@@ -0,0 +1,25 @@
+class MapviewerController < ApplicationController
+  def index
+
+
+
+hash = { a: true, b: false, c: nil}
+
+
+h1 = {'h2.1' => {'foo' => 'this', 'cool' => 'guy'}, 'h2.2' => {'bar' => '2000'} }
+
+
+
+
+  	 @hash = ['Country', 'Popularity'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700]
+
+
+  	render layout: "mapviewer"
+  end
+end
