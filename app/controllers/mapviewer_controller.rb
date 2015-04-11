47bdb8ea523d1984a315e3fc2fa7905791f752cd
Daniela Gattoni <danigattoni@gmail.com>
2015-04-11 15:22:03 -0300

    map viewe code commented in controller

--- app/controllers/mapviewer_controller.rb
+++ app/controllers/mapviewer_controller.rb
@@ -3,24 +3,21 @@ class MapviewerController < ApplicationController
 
 
 
-hash = { a: true, b: false, c: nil}
+# hash = { a: true, b: false, c: nil}
 
 
-h1 = {'Germany' => {'foo' => 'this', 'cool' => 'guy'}, 
-'h2.2' => {'bar' => '2000'} 
+# h1 = {'Germany' => {'foo' => 'this', 'cool' => 'guy'}, 
+# 'h2.2' => {'bar' => '2000'} 
 
-}
+# }
 
-
-
-
-  	 @hash = ['Country', 'Popularity'],
-          ['Germany', 200],
-          ['United States', 300],
-          ['Brazil', 400],
-          ['Canada', 500],
-          ['France', 600],
-          ['RU', 700]
+# @hash = ['Country', 'Popularity'],
+#     ['Germany', 200],
+#     ['United States', 300],
+#     ['Brazil', 400],
+#     ['Canada', 500],
+#     ['France', 600],
+#     ['RU', 700]
 
 
   	render layout: "mapviewer"
